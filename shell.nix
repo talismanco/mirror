e6d1ac23aba2e126c0fb3824678f147373f139d8
Sam Craig <contact@samcraig.io>
2020-10-02 12:18:20 -0400

    build: add nodejs to buildInputs of shell.nix

--- shell.nix
+++ shell.nix
@@ -8,12 +8,13 @@ in
 nixpkgs.mkShell rec {
   name = "mirror";
   env = nixpkgs.buildEnv { name = name; paths = buildInputs; };
-  buildInputs =[
+  buildInputs = [
     # <lunarispkgs>
     lunarispkgs.go_1_14_4
     lunarispkgs.golangci-lint_1_27_0
     lunarispkgs.helm_3_2_1
     lunarispkgs.jq_1_6
+    lunarispkgs.nodejs_12_18_3
     lunarispkgs.python_3_7_7
     lunarispkgs.skaffold_1_10_1
     # <nixpkgs>
