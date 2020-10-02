add786c979def830c68a0cfd20768ebfdddfcb92
Sam Craig <contact@samcraig.io>
2020-10-02 10:55:25 -0400

    style: add  prefix to respective package buildInputs

--- shell.nix
+++ shell.nix
@@ -8,13 +8,16 @@ in
 nixpkgs.mkShell rec {
   name = "mirror";
   env = nixpkgs.buildEnv { name = name; paths = buildInputs; };
-  buildInputs = with lunarispkgs; [
-    go_1_14_4
-    golangci-lint_1_27_0
-    helm_3_2_1
-    jq_1_6
-    python_3_7_7
-    skaffold_1_10_1
+  buildInputs =[
+    # <lunarispkgs>
+    lunarispkgs.go_1_14_4
+    lunarispkgs.golangci-lint_1_27_0
+    lunarispkgs.helm_3_2_1
+    lunarispkgs.jq_1_6
+    lunarispkgs.python_3_7_7
+    lunarispkgs.skaffold_1_10_1
+    # <nixpkgs>
+    # ...
   ];
   shellHook = "unset GOPATH";
-}
+} 
