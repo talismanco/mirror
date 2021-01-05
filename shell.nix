a2bc2f753f1041371c80b35dcc93ff344f0fae86
Sam Craig <contact@samcraig.io>
2021-01-05 12:26:53 -0500

    build: update go version to 1.14.13

--- shell.nix
+++ shell.nix
@@ -10,7 +10,7 @@ nixpkgs.mkShell rec {
   env = nixpkgs.buildEnv { name = name; paths = buildInputs; };
   buildInputs = [
     # <talismanpkgs>
-    talismanpkgs.go_1_14_4
+    talismanpkgs.go_1_14_13
     talismanpkgs.golangci-lint_1_27_0
     talismanpkgs.nodejs_12_18_3
     talismanpkgs.python_3_7
